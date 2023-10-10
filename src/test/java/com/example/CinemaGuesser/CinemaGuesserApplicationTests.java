566bac649e138b937a8651f765a55b09eea30553
druzhinin <saske12321@gmail.com>
2023-10-10 12:09:32 +0700

    Init project

---
+++ src/test/java/com/example/CinemaGuesser/CinemaGuesserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CinemaGuesser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaGuesserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
