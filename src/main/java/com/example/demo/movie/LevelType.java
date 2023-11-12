9778fd39e4773c763191512ef9b73d0a0e995b76
Ivan2593 <i.drobyshevskii@g.nsu.ru>
2023-11-12 23:58:15 +0700

    получение фильма из IMDb

---
+++ src/main/java/com/example/demo/movie/LevelType.java
@@ -0,0 +1,8 @@
+package com.example.demo.movie;
+
+public enum LevelType {
+    EASY,
+    NORMAL,
+    HARD,
+
+}
---
+++ src/main/java/com/example/demo/movie/ParameterType.java
@@ -0,0 +1,13 @@
+package com.example.demo.movie;
+
+public enum ParameterType {
+    ACTOR,
+    DIRECTOR,
+    GENRE,
+    KEYWORD,
+    IMAGES,
+    YEARS,
+    RATING,
+    COUNTRIES,
+
+}
